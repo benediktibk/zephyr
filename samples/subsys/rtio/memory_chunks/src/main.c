00d0ec3f2da702454e5613cbd5c0c4f801c632d8
Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>
2025-04-03 07:49:21 +0200

    add basics for new RTIO sample
    Signed-off-by: Benedikt Schmidt <benedikt.schmidt@embedded-solutions.at>

---
+++ samples/subsys/rtio/memory_chunks/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 SILA Embedded Solutions GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/rtio/rtio.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+int main(void)
+{
+	return 0;
+}
